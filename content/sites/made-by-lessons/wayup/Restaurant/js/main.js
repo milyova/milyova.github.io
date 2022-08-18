77093051bccfa1b61c597a0edb813170d5e26026
milyova <milyova@gmail.com>
2022-08-18 12:48:56 +0300

    Сайты полностью, десктопная версия

---
+++ content/sites/made-by-lessons/wayup/Restaurant/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.book').click(function () {
+        $('#modal2').addClass('show')
+    })
+})
+$('.modal2__overlay').click(function () {
+    $('#modal2').removeClass('show')
+})
