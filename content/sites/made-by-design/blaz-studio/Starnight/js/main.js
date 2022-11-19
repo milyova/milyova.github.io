cf53183b924d3a01a2191a956db369d107514989
milyova <milyova@gmail.com>
2022-11-19 16:19:25 +0300

    Все содержимое заменено на сайт тм - осень 2022

--- content/sites/made-by-design/blaz-studio/Starnight/js/main.js
+++
@@ -1,7 +0,0 @@
-const play = document.querySelector('.video__btn-play');
-const video = document.querySelector('.video');
-
-play.addEventListener('click', () => {
-    video.play();
-    play.classList.add('.video__btn-play--hidden');
-});
--- content/sites/made-by-lessons/wayup/Restaurant/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.book').click(function () {
-        $('#modal2').addClass('show')
-    })
-})
-$('.modal2__overlay').click(function () {
-    $('#modal2').removeClass('show')
-})
