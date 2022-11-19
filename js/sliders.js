fa3c958448939592e63d1fdd4e764a113d7bd470
milyova <milyova@gmail.com>
2022-11-19 16:28:56 +0300

    Добавить тм - осень 2022

---
+++ js/sliders.js
@@ -0,0 +1,28 @@
+// slider WE-TECH
+$(document).ready(function(){
+    $('.slider-we-tech').slick({
+      arrows:true,
+      slidesToShow: 3,
+      swipe: true,
+      touchThreshold: 10,
+      waitForAnimate: false,
+      centerMode: false,
+      variableWidth: true,   
+    });
+  });
+  // end slider WE-TECH
+  
+  // slider FAMOUS ABOUT TM
+  $(document).ready(function(){
+    $('.slider__famous-about-tm').slick({
+      arrows: true,
+      slidesToShow: 1,
+      swipe: true,
+      touchThreshold: 10,
+      waitForAnimate: false,
+      centerMode: true,
+      variableWidth: true,   
+    });
+  });
+  // end slider FAMOUS ABOUT TM
+  
