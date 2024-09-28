5838eb3421d50b80678f44e60f495f9da1879dbd
Tatiana Milyova <milyova@gmail.com>
2024-09-28 17:38:00 +0300

    JavaScript add repository

--- js/video_files.js
+++
@@ -1,8 +0,0 @@
-const play = document.querySelector('.btn__play');
-const video = document.querySelector('.video_file video');
-
-play.addEventListener('click', () => {
-    video.play();
-    video.setAttribute('controls', 'controls');
-    play.classList.add('btn__play--hidden');
-});
