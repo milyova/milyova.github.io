f60cffb8d7bdaff9b050933bdcc562f7aabe6dd8
milyova <108426832+milyova@users.noreply.github.com>
2025-02-17 22:09:20 +0300

    Update navbar.js

--- timer-new-year/js/components/navbar.js
+++ timer-new-year/js/components/navbar.js
@@ -8,23 +8,23 @@ let btnNewYear = document.getElementById('timer-seasons-bg--new-year');
 
 // смена фона по клику - Фон весна
 btnBgSpring.addEventListener('click', function () {
-    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../images/bg_spring.jpg)";
+    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../../images/bg_spring.jpg)";
 });
 // смена фона по клику - Фон лето
 btnBgSummer.addEventListener('click', function () {
-    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../images/bg_summer.jpg)";
+    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../../images/bg_summer.jpg)";
 });
 // смена фона по клику - Фон осень
 btnBgAutumn.addEventListener('click', function () {
-    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../images/bg_autumn.jpg)";
+    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../../images/bg_autumn.jpg)";
 });
 // смена фона по клику - Фон зима
 btnBgWinter.addEventListener('click', function () {
-    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../images/bg_winter.jpg)";
+    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../../images/bg_winter.jpg)";
 });
 // смена фона по клику - Фон декабрь
 btnBgDecember.addEventListener('click', function () {
-    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../images/bg_forest1.jpg)";
+    document.getElementById('countdown--seasons-bg').style.backgroundImage = "url(../../images/bg_forest1.jpg)";
 });
 // для всех кнопок, кроме новогодней ночи, переключение с поздравления на обратный отсчет
 let menuButtonsOnTimer = [btnBgSpring, btnBgSummer, btnBgAutumn, btnBgWinter, btnBgDecember];
